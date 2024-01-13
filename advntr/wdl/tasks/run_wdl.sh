0abb830c7831b981afbdf02ce68a998e39d193c5
sara-javadzadeh <saraj@eng.ucsd.edu>
2024-01-12 20:38:44 -0800

    Add working workflow for genotyping. Add database to dockerfile. Fix docker image installation issue.

--- advntr/wdl/tasks/run_wdl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# run the following to check the syntax of a wdl file:
-#java -jar ~/packages/wdl/womtool-47.jar validate hello.wdl
-
-java -jar ~/packages/wdl/cromwell-47.jar run advntr.wdl > wdl_output
