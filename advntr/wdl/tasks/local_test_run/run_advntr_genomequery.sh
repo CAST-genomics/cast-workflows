41fb8f8e81b8c63a27fa0c3f2ad2738949249081
sara-javadzadeh <saraj@eng.ucsd.edu>
2024-01-18 22:55:57 -0800

    Reorganized files

--- advntr/wdl/tasks/local_test_run/run_advntr_genomequery.sh
+++ advntr/wdl/tasks/local_test_run/run_advntr_genomequery.sh
@@ -4,4 +4,4 @@
 #java -jar ~/packages/wdl/womtool-47.jar validate hello.wdl
 
 # Updated version as of 1/11/2024
-java  -jar ~/packages/cromwell-86/cromwell-86.jar run advntr.wdl --inputs genome_browser_inputs.json &> wdl_output.txt
+java  -jar ~/packages/cromwell-86/cromwell-86.jar run advntr.wdl --inputs genomequery_inputs.json &> wdl_output.txt
