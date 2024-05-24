2d49e0c65398e7401981969a0257d75433347f55
sara-javadzadeh <sarajno@gmail.com>
2024-05-24 22:29:49 +0000

    Add convert_to_bref.sh

--- imputation/wdl/download_jar_files.sh
+++ imputation/wdl/download_jar_files.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 wget "https://faculty.washington.edu/browning/beagle/beagle.01Mar24.d36.jar"
+wget "https://faculty.washington.edu/browning/beagle/bref3.19Apr22.7c0.jar"
+wget "https://faculty.washington.edu/browning/beagle/unbref3.19Apr22.7c0.jar"
 wget "https://github.com/broadinstitute/cromwell/releases/download/87/cromwell-87.jar"
