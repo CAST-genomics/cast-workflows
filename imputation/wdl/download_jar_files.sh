03f8b638c8c655d8cea5353d8f04165ad4d9f6f2
sara-javadzadeh <sarajno@gmail.com>
2024-05-24 22:48:51 +0000

    update jar files to download

--- imputation/wdl/download_jar_files.sh
+++ imputation/wdl/download_jar_files.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# Beagle
 wget "https://faculty.washington.edu/browning/beagle/beagle.01Mar24.d36.jar"
+wget "https://faculty.washington.edu/browning/beagle/beagle.19Apr22.7c0.jar"
+
+# Bref
 wget "https://faculty.washington.edu/browning/beagle/bref3.19Apr22.7c0.jar"
+wget "https://faculty.washington.edu/browning/beagle/bref3.01Mar24.d36.jar"
+
+# Unbref
 wget "https://faculty.washington.edu/browning/beagle/unbref3.19Apr22.7c0.jar"
+wget "https://faculty.washington.edu/browning/beagle/unbref3.01Mar24.d36.jar"
+
+#Cromwell
 wget "https://github.com/broadinstitute/cromwell/releases/download/87/cromwell-87.jar"
