8d71b76e6a7e3be590a32617bbd8afe725046a58
sara-javadzadeh <sarajno@gmail.com>
2024-05-24 22:29:49 +0000

    Add convert_to_bref.sh

---
+++ imputation/wdl/convert_vcf_to_bref.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+chr="chr22"
+new_bref="bref3.01Mar24.d36.jar"
+old_bref="bref3.19Apr22.7c0.jar"
+bref=$new_bref
+
+zcat data/${chr}_final_SNP_merged_additional_TRs.vcf.gz | \
+	java -jar $bref > data/${chr}_final_SNP_merged_additional_TRs.bref3
--- imputation/wdl/run_beagle_local.sh
+++ imputation/wdl/run_beagle_local.sh
@@ -3,7 +3,8 @@
 chr="chr21"
 #gt="data/ALL.${chr}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes_bialleleic_uniq_id_name.sorted.vcf.gz"
 gt="data/ALL.${chr}.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased_chr.vcf.gz"
-ref="data/${chr}_final_SNP_merged_additional_TRs.vcf.gz"
+#ref="data/${chr}_final_SNP_merged_additional_TRs.vcf.gz"
+ref="data/${chr}_final_SNP_merged_additional_TRs.bref3"
 
 beagle_5_4="beagle.01Mar24.d36.jar"
 beagle_5_4_old="beagle.19Apr22.7c0.jar"
