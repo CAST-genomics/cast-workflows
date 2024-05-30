5406b560f8d8e4b654bb662f0bc04dca3895a542
sara-javadzadeh <sarajno@gmail.com>
2024-05-30 18:05:46 +0000

    add create reference wdl and bash file. it works

--- imputation/wdl/run_create_reference.sh
+++ imputation/wdl/run_create_reference.sh
@@ -3,9 +3,10 @@
 region="chr15:83855424-93857434"
 snp_vcf="gs://fc-aou-datasets-controlled/v7/wgs/long_read/joint_vcf/GRCh38/cohort_for_GLNexus_2023Q1_1027.g.vcf.bgz"
 vntr_vcf="$WORKSPACE_BUCKET/saraj/vntr_reference_panel/ACAN_merged_samples.sorted.vcf.gz"
-
-python create_reference.py \
+date
+time python create_reference.py \
 	--name tr_build_reference \
 	--snp-vcf $snp_vcf \
 	--vntr-vcf $vntr_vcf \
 	--region $region
+date
