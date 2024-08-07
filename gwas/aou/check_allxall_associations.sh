0c877a03b533d962f024012ec61c738e55ef433c
sara-javadzadeh <jupyter@all-of-us-6359-m.us-central1-c.c.terra-vpc-sc-ba43050e.internal>
2024-08-07 22:59:54 +0000

    All-by-all query works now. cleanup.

--- gwas/aou/check_allxall_associations.sh
+++ gwas/aou/check_allxall_associations.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# ACAN VNTR chr15:88855424-88857434
+# 10Mbp region is chr15:83855424-93857434
+# 100Mbp region is chr15:1-100000000
+# 1Mbp region is chr15:88355424-89357434
+region="chr15:83855424-93857434"
+region="chr15:1-100000000"
 python check_allxall_associations.py \
 	--type ACAF \
         --pop META \
-	--phenoname height
+	--phenoname height \
+	--region $region
