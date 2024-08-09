4639bbf2eb7dbdb540d299d3e5a250cf828978fb
sara-javadzadeh <sarajno@gmail.com>
2024-08-09 01:13:25 +0000

    Add --local option to query_allxall. Fix manhattan plot in query_allxall.

--- gwas/aou/query_allxall_associations.py
+++ gwas/aou/query_allxall_associations.py
@@ -8,6 +8,7 @@ import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 import hail as hl
+import numpy as np
 
 """
 All hail files are located on gs://fc-aou-datasets-controlled/AllxAll/v1/
@@ -31,6 +32,8 @@ def parse_args():
                             type=str, required=True)
     arg_parser.add_argument("--region", help="Region to look for SNP-phenotype associations (chr:start-end)",
                             type=str, required=True)
+    arg_parser.add_argument("--local", help="Run based on the latest local run, instead of a hail query",
+                            action="store_true")
     args = arg_parser.parse_args()
     return args
 
@@ -47,7 +50,7 @@ def get_hail_path(ex_type, pop, phenoname):
 def init():
     hl.init(default_reference = "GRCh38")
 
-def annotate_points(plot, population, region_chrom):
+def annotate_points(data, plot, population, region_chrom):
     ax = plot.ax
     points = [ \
             ("chr15", 88855424, "ACAN VNTR", {"AFR": 67.44,
@@ -66,49 +69,74 @@ def annotate_points(plot, population, region_chrom):
         # Pick the annotation passed by the user
         if region_chrom != chrom:
             continue
-        x = position
+        index = len(data[data["POS"] < position])
+        x = index
         y = p_value_dict[population]
         ax.text(x=x, y=y, s=label, fontsize="medium")
-        ax.scatter(x=position, y=y, color="red", marker="^")
+        ax.scatter(x=x, y=y, color="red", marker="^")
 
-def plot_figures(ht, args, region_chrom):
-    ## plot a manhattan plot
-    filename = "figures/allxall_region_{}_pop_{}_manhattan.png".format(args.phenoname, args.pop)
+def plot_figures(data, args, region_chrom,
+                 p_value_threshold=-np.log10(5*10**-8)):
+    
+    # Figure filename
+    filename = "figures/allxall_manhattan_{}_pop_{}.png".format(args.phenoname, args.pop)
     title = 'Region P-value for phenotype {}'.format(args.phenoname)
-    data = ht.to_pandas()
-    plot = sns.relplot(data=data, x="POS", y="Pvalue_log10",
+    data['ind'] = data.index
+    # Plot a Manhattan plot based on the dataframe
+    plot = sns.relplot(data=data, x="ind", y="Pvalue_log10",
         s=30, aspect=4, linewidth=0, hue="CHR", palette="tab10", legend=None)
-    annotate_points(plot, args.pop, region_chrom)
-    plot.fig.savefig(filename)
+    chrom_df = data.groupby("CHR")["ind"].median()
+
+    # Set labels
+    plot.ax.set_xlabel("Chromosome")
+    plot.ax.set_xticks(chrom_df)
+    plot.ax.set_xticklabels(chrom_df.index)
+    annotate_points(data, plot, args.pop, region_chrom)
+
+    # Put the threshold
+    plot.ax.axhline(p_value_threshold, linestyle="--", linewidth=1)
+    plot.fig.savefig(filename, bbox_inches="tight")
 
     ## Plot effect sizes
     plt.clf()
-    filename = "figures/allxall_{}_pop_{}_effect_sizes.png".format(args.phenoname, args.pop)
+    filename = "figures/allxall_effect_sizes_{}_pop_{}.png".format(args.phenoname, args.pop)
     title = 'Region effect sizes for phenotype {}'.format(args.phenoname)
-    plot = sns.relplot(data=data, x="POS", y="BETA",
+    plot = sns.relplot(data=data, x="ind", y="BETA",
         s=30, aspect=4, linewidth=0, hue="CHR", palette="tab10", legend=None)
-    annotate_points(plot, args.pop, region_chrom)
-    plot.fig.savefig(filename)
+    annotate_points(data, plot, args.pop, region_chrom)
+    plot.fig.savefig(filename, bbox_inches="tight")
 
 
 def main():
     args = parse_args()
     #init()
 
-    # Get the corresponding hail path
-    ht_path, mt_path = get_hail_path(ex_type=args.type,
-                            pop=args.pop,
-                            phenoname=args.phenoname)
-    ht = hl.read_table(ht_path)
     chrom, start_end = args.region.split(":")
     start, end = start_end.split("-")
-    ht = ht.filter(hl.all(
-           ht.CHR == chrom,
-           ht.POS > int(start),
-           ht.POS < int(end)
-       ))
-    print("len of hail table after filtering: {}".format(ht.count()))
-    plot_figures(ht, args, chrom)
+    if not args.local:
+        # Get the corresponding hail path
+        ht_path, mt_path = get_hail_path(ex_type=args.type,
+                                pop=args.pop,
+                                phenoname=args.phenoname)
+        # Read the hail table and filter according to the region
+        ht = hl.read_table(ht_path)
+        ht = ht.filter(hl.all(
+               ht.CHR == chrom,
+               ht.POS > int(start),
+               ht.POS < int(end)
+           ))
+        print("len of hail table after filtering: {}".format(ht.count()))
+        # Save the dataframe for a later local run.
+        data = ht.to_pandas()
+        data.to_csv("outputs/df_dump.csv")
+    else:
+        # Load data locally
+        data = pd.read_csv("outputs/df_dump.csv")
+        data = data[((data["CHR"] == chrom) & \
+                     (data["POS"] > int(start)) & \
+                     (data["POS"] < int(end)))]
+
+    plot_figures(data=data, args=args, region_chrom=chrom)
 
 
 if __name__ == "__main__":
