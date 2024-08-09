9974e638fcdb5d4280f45e9499718d127ca09893
sara-javadzadeh <sarajno@gmail.com>
2024-08-09 01:13:25 +0000

    Add --local option to query_allxall. Fix manhattan plot in query_allxall.

--- gwas/aou/run_query_allxall.sh
+++ gwas/aou/run_query_allxall.sh
@@ -6,35 +6,31 @@
 # 1Mbp region is chr15:88355424-89357434
 #region="chr15:83855424-93857434"
 #region="chr15:1-100000000"
+#phenotype="height"
 
 # INS VNTR
 # 1mbp region
-region="chr11:1661570-2661976"
+#region="chr11:1661570-2661976"
 # 10mbp region
-region="chr11:1-12161976"
-
-#CUL4A VNTR chr13:113231980-113232318
-region="chr13:1-114000000"
-
-phenotype="height"
-
+#region="chr11:1-12161976"
+# whole chromosome
+#region="chr11:1-135127769"
 # Type 2 Diabetes, 2 phenonames
-#phenotype="EM_202.2"
-phenotype="250.2"
+#for phenotype in"EM_202.2" "250.2"; do
 
-# mean corpuscular hemoglobin concentration
-phenotype="3009744"
-python query_allxall_associations.py \
+# mean corpuscular hemoglobin concentration and CUL4A VNTR
+#CUL4A VNTR chr13:113231980-113232318
+#region="chr13:113337980-113272318"
+region="chr13:1-114000000"
+for phenotype in "3009744" "GE_970"; do
+
+  python query_allxall_associations.py \
 	--type ACAF \
         --pop META \
 	--phenoname $phenotype \
+	--local \
 	--region $region
-phenotype="GE_970"
-python query_allxall_associations.py \
-	--type ACAF \
-        --pop META \
-	--phenoname $phenotype \
-	--region $region
+done
 exit 0
 python query_allxall_associations.py \
 	--type ACAF \
