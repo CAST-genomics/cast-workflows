c5ba2340fa79f900dab0a4e79b4bd173ff764eb0
sara-javadzadeh <sarajno@gmail.com>
2024-08-12 22:03:03 +0000

    Add the remaining VNTRs in the list

--- gwas/aou/run_query_allxall.sh
+++ gwas/aou/run_query_allxall.sh
@@ -89,8 +89,56 @@
 
 # age of onset BD and PER3 VNTR
 # PER3 VNTR chr1:7829873-7830144
-region="chr1:2829873-12830144"
-phenotypes="MB_286.1 296.1 MB_286.11 MB_286.12 836803"
+#region="chr1:2829873-12830144"
+#phenotypes="MB_286.1 296.1 MB_286.11 MB_286.12 836803"
+
+# ALS and C9orf72 VNTR
+# C9orf72  VNTR chr9:27573484-27573555
+#region="chr9:22573484-32573555" #10mbp region
+#phenotypes="NS_328 290" # Dementias and cerebral degeneration
+#phenotypes="NS_328.1 290.1" # Dementias < 900 individuals
+#phenotypes="N06D" # Anti dimentia drugs
+#phenotypes="NS_328 290 NS_328.1 290.1 N06D"
+
+
+# Dystonia and PK and TAF1 VNTR
+# TAF1 VNTR chrX:71453055-71453129
+#region="chrX:66453055-76453129" #10mbp
+#phenotypes="NS_324.31" # dystonia n_cases=869
+#phenotypes="NS_324.1 NS_324.11" # parkinson's disease n_cases=751 and 700
+#phenotypes="332" # parkinson's disease n_cases=300
+#phenotypes="N04" # Anti parkinson's drugs
+#phenotypes="N04 NS_324.31 NS_324.1 NS_324.11 332"
+
+# Epilepsy and CSTB VNTR
+# CSTB VNTR chr21:43776443-43776479
+#region="chr21:38776443-48776479" #10mbp
+#phenotypes="NS_330 345 345.1 NS_330.1" # epilepsy n=5300 and 4300 and 1500
+#phenotypes="NS_330.12 345.12" # partial epilepsy n=800, 400
+# NS_330.11 is 200 for partial epilepsy.
+# 836852 is 200 for PFHH
+#phenotypes="NS_330 345 345.1 NS_330.1 NS_330.12 345.12"
+
+
+# Dimentia and PRNP VNTR
+# PRNP VNTR chr20:4699397-4699493
+#region="chr20:1-9699493" #<10mbp
+#phenotypes="NS_328 290 NS_328.1 290.1 N06D"
+
+# motor neuropathy and VWA1
+# VWA1 VNTR chr1:1435799-1435818
+region="chr1:1-6435818" # 5mbp region
+#phenotypes="GE_972.1" # Hereditary neuropathy n=3072
+#phenotypes="357" # Inflammatory and toxic neuropathy n=1700
+# 250.6 #Polyneuropathy in diabetes n=1568
+# 356 Hereditary and idiopathic peripheral neuropathy n_cases=1319
+# SO_380.1 Optic neuropathy n=1028
+# 836811 PFHH
+# NS_337.3 Toxic polyneuropathy n=500
+# 377.3 Optic neuritis/neuropathy n=400
+# NS_343.1 Peripheral autonomic neuropathy n=265
+# NS_337.6 Idiopathic progressive neuropathy n=229
+phenotypes="GE_972.1 357 250.6 356 SO_380.1 836811 NS_337.3 377.3"
 
 for phenotype in $phenotypes; do
   python query_allxall_associations.py \
