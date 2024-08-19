70ba7ea9e4ec36e403d37c8e19497673464c5f8a
sara-javadzadeh <sarajno@gmail.com>
2024-08-19 19:34:29 +0000

    create_ref rewritten. Now phasing and bref is included. Including beagle features and updates similar to imputation pipeline. Major cleanup.

--- imputation/wdl/debug_missing_shards/find_missing_shards.sh
+++ imputation/wdl/debug_missing_shards/find_missing_shards.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-workflow="a5d62008-af2c-4474-b8c0-d86f3b068d73"
-workflow="6a859277-68af-4f8b-a497-a5f5833b4fb5"
-gsutil ls gs://fc-secure-f6524c24-64d9-446e-8643-415440f52b46/cromwell-execution/imputation_batch/${workflow}/call-imputation/shard-*/imputation/*/call-sort_index_beagle/cacheCopy/batch_set1_output.sorted_TR.vcf.gz > list_items.txt
-gsutil ls gs://fc-secure-f6524c24-64d9-446e-8643-415440f52b46/cromwell-execution/imputation_batch/${workflow}/call-imputation/shard-*/imputation/*/call-sort_index_beagle/batch_set1_output.sorted_TR.vcf.gz >> list_items.txt
+workflow="4429a2a9-09d6-4433-87ca-2b1c9efb8cb0"
+#gsutil ls $WORKSPACE_BUCKET/cromwell-execution/imputation_batch/${workflow}/call-imputation/shard-*/imputation/*/call-sort_index_beagle/cacheCopy/batch_set1_output.sorted_TR.vcf.gz > list_items.txt
+gsutil ls $WORKSPACE_BUCKET/cromwell-execution/imputation_batch/${workflow}/call-imputation/shard-*/imputation/*/call-sort_index_beagle/batch_set1_output.sorted_TR.vcf.gz > list_items.txt
 
 cat list_items.txt | awk -F/ '{print ($8)}' | awk -F- '{print($2)}' | sort -n > list_items_shard_number.txt
 
--- imputation/wdl/run_create_reference.sh
+++ imputation/wdl/run_create_reference.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-region="chr15:83855424-93857434"
+chr="chr15"
+
+#window=20
+window=2
+#regions="$WORKSPACE_BUCKET/saraj/vntr_reference_panel/ACAN_region_50m.bed"
+regions="$WORKSPACE_BUCKET/saraj/vntr_reference_panel/ACAN_region_4m.bed"
+
 snp_vcf="gs://fc-aou-datasets-controlled/v7/wgs/long_read/joint_vcf/GRCh38/cohort_for_GLNexus_2023Q1_1027.g.vcf.bgz"
 vntr_vcf="$WORKSPACE_BUCKET/saraj/vntr_reference_panel/ACAN_merged_samples.sorted.vcf.gz"
 sample_list="$WORKSPACE_BUCKET/saraj/vntr_reference_panel/vntr_samples_650_train.txt"
+map="$WORKSPACE_BUCKET/saraj/genetic_map/plink.${chr}_w_chr.GRCh38.map"
+
 date
 time python create_reference.py \
-	--name vntr_reference_650 \
+	--name vntr_ref_test \
 	--snp-vcf $snp_vcf \
 	--vntr-vcf $vntr_vcf \
-	--region $region \
-	--samples $sample_list
+	--regions $regions \
+	--samples $sample_list \
+        --window $window \
+	--chrom $chr \
+	--mem 20 \
+	--cromwell \
+	--map $map
+
 date
