a3cb4d917f6a2e2332a6242b2f02274f6b1a731a
sara-javadzadeh <sarajno@gmail.com>
2024-08-19 22:48:43 +0000

    Fix errors for create_ref. Using bref docker now. tested on small sample size up to (before) bref

--- imputation/wdl/run_create_reference.sh
+++ imputation/wdl/run_create_reference.sh
@@ -2,14 +2,17 @@
 
 chr="chr15"
 
+#mem=20
+mem=4
 #window=20
-window=2
+window=3
 #regions="$WORKSPACE_BUCKET/saraj/vntr_reference_panel/ACAN_region_50m.bed"
-regions="$WORKSPACE_BUCKET/saraj/vntr_reference_panel/ACAN_region_4m.bed"
+regions="$WORKSPACE_BUCKET/saraj/vntr_reference_panel/ACAN_region_10m.bed"
 
 snp_vcf="gs://fc-aou-datasets-controlled/v7/wgs/long_read/joint_vcf/GRCh38/cohort_for_GLNexus_2023Q1_1027.g.vcf.bgz"
 vntr_vcf="$WORKSPACE_BUCKET/saraj/vntr_reference_panel/ACAN_merged_samples.sorted.vcf.gz"
-sample_list="$WORKSPACE_BUCKET/saraj/vntr_reference_panel/vntr_samples_650_train.txt"
+#sample_list="$WORKSPACE_BUCKET/saraj/vntr_reference_panel/vntr_samples_650_train.txt"
+sample_list="$WORKSPACE_BUCKET/saraj/vntr_reference_panel/vntr_samples_20_small_test.txt"
 map="$WORKSPACE_BUCKET/saraj/genetic_map/plink.${chr}_w_chr.GRCh38.map"
 
 date
@@ -21,7 +24,7 @@ time python create_reference.py \
 	--samples $sample_list \
         --window $window \
 	--chrom $chr \
-	--mem 20 \
+	--mem $mem \
 	--cromwell \
 	--map $map
 
