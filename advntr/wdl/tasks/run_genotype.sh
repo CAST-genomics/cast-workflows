df1a94d3d41dd444c4b211f9b268e72f992e32bf
sara-javadzadeh <sarajno@gmail.com>
2024-08-20 21:41:20 +0000

    Add memory feature to advntr wdl. Remove unnecessary maxretries. Fix conting in the header of vcf files. Fix samtools error. Cleanup

--- advntr/wdl/tasks/run_genotype.sh
+++ advntr/wdl/tasks/run_genotype.sh
@@ -2,14 +2,17 @@
 
 # Batch size was 50 on the ACAN run
 batch_size=50
+mem=4
 output="acan_replicate"
 region_file="$WORKSPACE_BUCKET/saraj/vntr_reference_panel/ACAN_region_1k_each_side.bed"
 vntr_id="290964"
 
 time python genotype_aou.py \
   --dataset lrwgs \
-  --sample-count 5 \
+  --sample-count 4 \
   --batch-size 2 \
   --output-name $output \
   --region-file $region_file \
+  --cromwell \
+  --mem $mem \
   --vntr-id $vntr_id
