3ba9a23c795726ac4eeb8334a60bddefb571d8ee
sara-javadzadeh <sarajno@gmail.com>
2024-08-30 23:42:48 +0000

    Print the number of filtered calls in filter_calls.py. Try varied values for sr_threshold and ml_threshold

--- advntr/wdl/tasks/filter_calls.py
+++ advntr/wdl/tasks/filter_calls.py
@@ -4,6 +4,9 @@ import statistics
 from matplotlib import pyplot as plt
 
 def filter_vcf(filename, sr_threshold, ml_threshold, out_filename):
+    num_updated_calls = 0
+    num_calls = 0
+    num_prev_no_call = 0
     with open(out_filename, "w+") as output_file:
         with open(filename, "r") as input_file:
             for line in input_file.readlines():
@@ -21,24 +24,37 @@ def filter_vcf(filename, sr_threshold, ml_threshold, out_filename):
                         continue
                     max_likelihood = float(word.split(":")[4])
                     spanning_read = int(word.split(":")[2])
-                    if max_likelihood >= ml_threshold and \
+                    if word == "./.:0:0:0:0":
+                        num_calls += 1
+                        num_prev_no_call += 1
+                    elif max_likelihood >= ml_threshold and \
                        spanning_read >= sr_threshold:
                         # Passing filters
                         output_words.append(word)
+                        num_calls += 1
                     else:
                         # Not passing filters. Replace with a no call.
                         output_words.append("./.:0:0:0:0")
+                        num_updated_calls += 1
+                        num_calls += 1
                 
                 # Fix the spacing
                 output_line = "\t".join(output_words)
                 output_file.write(output_line + '\n')
+    print("Filter done with sr_threshold {} ml_threshold {}.".format(
+                sr_threshold, ml_threshold))
+    print("{} calls were already no call. In addition, {} calls removed ({:.4}% all calls)".format(
+                num_prev_no_call,
+                num_updated_calls,
+                float(num_updated_calls)/num_calls*100,
+                ))
 
 if __name__ == "__main__":
     vcf = "data/merged_samples.sorted_batch_1.vcf"
     out_vcf = "data/filtered_merged_samples.sorted_batch_1.vcf"
-    sr_threshold = 4
-    ml_threshold = 0.9
-    filter_vcf(filename=vcf,
-               sr_threshold=sr_threshold,
-               ml_threshold=ml_threshold,
-               out_filename=out_vcf)
+    for sr_threshold in [4, 6, 8]:
+        for ml_threshold in [0.9, 0.95]:
+            filter_vcf(filename=vcf,
+                   sr_threshold=sr_threshold,
+                   ml_threshold=ml_threshold,
+                   out_filename=out_vcf)
