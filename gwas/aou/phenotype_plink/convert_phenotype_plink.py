9bdc8c2f9b891c01955fda709160d5c8422c3a31
nichole <nicholema621@gmail.com>
2024-10-01 12:30:44 -0700

    add print statement

--- gwas/aou/phenotype_plink/convert_phenotype_plink.py
+++ gwas/aou/phenotype_plink/convert_phenotype_plink.py
@@ -96,6 +96,8 @@ def main():
     plink_pheno = plink[["FID","IID","phenotype"]]
     plink_pheno.to_csv(f"{args.phenotype}_pheno_plink.txt", sep="\t", index=False)
     data.to_csv(f"{args.phenotype}_covar_combined.txt", sep="\t", index=False)
+    
+    print("Done converting {args.phenotype} to plink format")
 
 
 if __name__ == "__main__":
