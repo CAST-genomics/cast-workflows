a9b41ff54876959e4cd1bf20b303ad37d3597894
sara-javadzadeh <sarajno@gmail.com>
2024-10-04 18:46:47 +0000

    Add a priliminary snomed option to process phenotypes. Save progress.

--- gwas/aou/phenotype_preprocessing/run_preprocess.sh
+++ gwas/aou/phenotype_preprocessing/run_preprocess.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+# The concept id for colon polyp is in SNOMED vocab
+# So the query is likely different from LOINC/PPI.
+# I'll have to change the query.
 
+python aou_phenotype_preprocessing.py \
+	--phenotype diabetes \
+   	--concept-id 201820 \
+   	--units "tmp" \
+        --snomed \
+   	--range 0,1
+#SNOMEDCode: 73211009
+#OMOP Concept Id: 201820
+exit 0
+python aou_phenotype_preprocessing.py \
+	--phenotype colon_polyp \
+   	--concept-id 4285898 \
+   	--units "tmp" \
+   	--range 0,1
+exit 0
 python aou_phenotype_preprocessing.py \
 	--phenotype height_ppi \
    	--concept-id 903133 \
@@ -8,11 +26,3 @@ python aou_phenotype_preprocessing.py \
    	--units centimeter \
    	--range 0,250 \
 
-# The concept id for colon polyp is in SNOMED vocab
-# So the query is likely different from LOINC/PPI.
-# I'll have to change the query.
-python aou_phenotype_preprocessing.py \
-	--phenotype colon_polyp \
-   	--concept-id 4285898 \
-   	--units "a" \
-   	--range 0,1 \
