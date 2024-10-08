1ae86d9aacdc65a355f2a5347420bf23f3ba551b
sara-javadzadeh <jupyter@all-of-us-6359-m.us-central1-c.c.terra-vpc-sc-ba43050e.internal>
2024-10-08 05:35:19 +0000

    Update region_phenotypes by adding blood phenotypes. Change query to store dataframe for all phenotypes

--- gwas/aou/run_query_allxall.py
+++ gwas/aou/run_query_allxall.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-
+from time import sleep
 
 def read_region_phenotypes(filename):
     data = pd.read_csv(filename)
@@ -9,18 +9,19 @@ def read_region_phenotypes(filename):
 def main():
     data = read_region_phenotypes("region_phenotypes.csv")
    
-    # Select a specific region and phenotype pair based on VNTR gene name
-    data = data[data["gene"] == "ACAN"]
-
-    for phenotype in data["phenotypes"][0].split():
-        command = "python query_allxall_associations.py " + \
-	"--type ACAF " + \
-        "--pop META " + \
-        "--local " + \
-	"--region {} ".format(data["region"][0]) + \
-	"--phenoname {} ".format(phenotype) + \
-	"--annotate annotation_points.csv"
-        os.system(command)
+    chroms = ["chr15:1-102000000", "chr11:1-136000000"]
+    for idx, row in data.iterrows():
+        print("-------- Running for phenotype ", row["phenotype_name"])
+        for chrom in chroms:
+            for phenotype in row["phenotypes"].split():
+                command = "python query_allxall_associations.py " + \
+                "--type ACAF " + \
+                "--pop META " + \
+                "--region {} ".format(chrom) + \
+                "--phenoname {} ".format(phenotype) + \
+                "--annotate annotation_points.csv"
+                os.system(command)
+        sleep(120)
 
 if __name__ == "__main__":
     main()
