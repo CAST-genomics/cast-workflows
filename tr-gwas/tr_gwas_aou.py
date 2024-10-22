8ed33b204626f52283b004eab3004920907bdc44
nichole <nicholema621@gmail.com>
2024-10-22 14:40:25 -0700

    fix bug

--- tr-gwas/tr_gwas_aou.py
+++ tr-gwas/tr_gwas_aou.py
@@ -14,8 +14,6 @@ import json
 import os
 import subprocess
 import sys
-import glob
-from google.cloud import storage
 
 sys.path.append("../utils")
 import aou_utils
@@ -31,15 +29,14 @@ def main():
 	parser.add_argument("--dryrun", help="Don't actually run the workflow. Just set up", action="store_true")
 	args = parser.parse_args()
 
-	storage_client = storage.Client()
-	bucket = storage_client.bucket(os.environ.get("WORKSPACE_BUCKET"))
+	
 	# Set up workflow JSON
 	json_dict = {}
 	json_dict["tr_gwas.pgens"] = os.environ.get("WORKSPACE_BUCKET") + "/tr_imputation/enstr-v3/pgen_list.txt"
 	json_dict["tr_gwas.psams"] = os.environ.get("WORKSPACE_BUCKET") + "/tr_imputation/enstr-v3/psam_list.txt"
 	json_dict["tr_gwas.pvars"] = os.environ.get("WORKSPACE_BUCKET") + "/tr_imputation/enstr-v3/pvar_list.txt"
-	json_dict["tr_gwas.phenotypes"] = [blob.name for blob in bucket.list_blobs(prefix="phenotypes/", delimiter="/") if blob.name.endswith('.csv')]
-	json_dict["tr_gwas.cohorts"] = [blob.name for blob in bucket.list_blobs(prefix="samples/", delimiter="/") if blob.name.endswith('.csv')]
+	json_dict["tr_gwas.phenotypes"] = os.environ.get("WORKSPACE_BUCKET") + "/phenotypes/phenotype_list.txt"
+	json_dict["tr_gwas.cohorts"] = os.environ.get("WORKSPACE_BUCKET") + "/samples/sample_list.txt"
 	
 	
 
