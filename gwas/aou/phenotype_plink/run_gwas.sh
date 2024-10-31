71fcb8849daa0e8f678dfbe82ecebc0eda8337a4
sara-javadzadeh <sarajno@gmail.com>
2024-10-31 00:55:23 +0000

    Fix conversion of binary phenotypes for plink

--- gwas/aou/phenotype_plink/run_gwas.sh
+++ gwas/aou/phenotype_plink/run_gwas.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 python convert_phenotype_plink.py \
-        --phenotype t2diabetes 
+        --phenotype t2diabetes \
+        --is-binary
                
 vcf="../../../imputation/wdl/data/sr_ml_filtered_ref_chr11_annotated"
 plink2 --pfile $vcf \
 	--pheno t2diabetes_pheno_plink.txt \
-	--linear \
+	--logistic \
 	--covar t2diabetes_covar_combined.txt \
 	--out chr11_t2diabetes_gwas \
 	--covar-variance-standardize
