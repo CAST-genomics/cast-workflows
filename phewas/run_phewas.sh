67eb3c752e1018f9e48eafb1a2c88bb182d64a79
sara-javadzadeh <sarajno@gmail.com>
2025-01-24 06:08:36 +0000

    Add argparse for run_phewas.py

--- phewas/run_phewas.sh
+++ phewas/run_phewas.sh
@@ -8,4 +8,8 @@
 ## To run the demo, run:
 #python3 -m PheTK.Demo
 
-python run_phewas.py
+#python run_phewas.py --locus "INS" --chrom "chr11" --start 2161569 --n-threads 12
+#python run_phewas.py --locus "ACAN" --chrom "chr15" --start 88855424 --n-threads 12
+#python run_phewas.py --locus "TMCO1" --chrom "chr1" --start 165761972 --n-threads 12
+python run_phewas.py --locus "SLC6A3" --chrom "chr5" --start 1393581 --n-threads 12
+python run_phewas.py --locus "EIF3H" --chrom "chr8" --start 116611155 --n-threads 12
