0afb935a76ec010f644e73265ec539252780066f
sara-javadzadeh <sarajno@gmail.com>
2025-01-31 22:05:05 +0000

    Cleanup phewas pipeline

--- phewas/run_phewas.sh
+++ phewas/run_phewas.sh
@@ -7,12 +7,13 @@
 
 ## To run the demo, run:
 #python3 -m PheTK.Demo
+tr_vcf="../imputation/wdl/data/imputed_chr11.annotated.rh.vcf.gz"
+python run_phewas.py --locus "INS" --chrom "chr11" --tr-vcf $tr_vcf --start 2161569 --n-threads 12
 
-#python run_phewas.py --locus "INS" --chrom "chr11" --start 2161569 --n-threads 12
 #python run_phewas.py --locus "ACAN" --chrom "chr15" --start 88855424 --n-threads 12
 #python run_phewas.py --locus "TMCO1" --chrom "chr1" --start 165761972 --n-threads 12
 #python run_phewas.py --locus "SLC6A3" --chrom "chr5" --start 1393581 --n-threads 12
 #python run_phewas.py --locus "EIF3H" --chrom "chr8" --start 116611155 --n-threads 12
 #python run_phewas.py --locus "NACA" --chrom "chr12" --start 56717495 --n-threads 12
 #python run_phewas.py --locus "CUL4A" --chrom "chr13" --start 113231979 --n-threads 12
-python run_phewas.py --locus "NACA" --chrom "chr12" --start 56717495 --n-threads 12 --min-phecode-count 4
+#python run_phewas.py --locus "NACA" --chrom "chr12" --start 56717495 --n-threads 12 --min-phecode-count 4
