96e5f6a7f903f11eae4b0fd3adb520fb4d23f9ff
nichole <nicholema621@gmail.com>
2025-02-06 17:15:49 -0800

    add script to run pheTK using region file

---
+++ phewas/run_aou_pheTK.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## We use PheTK to run phewas. For more information on installation and documentation see https://github.com/nhgritctran/PheTK/tree/main?tab=readme-ov-file
+
+## To install run:
+#pip install PheTK --upgrade
+
+#!/bin/bash
+
+# Path to the region file and tr_vcf file
+region_file="$1"
+tr_vcf="$2"
+
+# Ensure the region file exists
+if [[ ! -f "$region_file" ]]; then
+    echo "Region file does not exist."
+    exit 1
+fi
+
+# Iterate over each line in the region file
+while IFS= read -r line; do
+    # Remove leading and trailing whitespaces
+    region=$(echo "$line" | xargs)
+    
+    # Run the Python script with the provided arguments
+    python run_phewas.py --region "$region" --tr-vcf "$tr_vcf" --n-threads 12 --out "$region_pheTK"  
+done < "$region_file"
