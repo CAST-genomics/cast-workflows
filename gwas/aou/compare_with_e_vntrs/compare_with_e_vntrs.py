caf1ab04cc615eb09d9d73dfa31edea61e57080e
sara-javadzadeh <sarajno@gmail.com>
2025-05-03 00:35:02 +0000

    Add script to compare significant hits with previously found e_vntrs

---
+++ gwas/aou/compare_with_e_vntrs/compare_with_e_vntrs.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+
+def read_e_vntrs(filename):
+    input_file = pd.ExcelFile(filename)
+    data = pd.read_excel(input_file, "VNTRs")
+    #data = data[["VNTR Coordinates (GRCh38)"]]
+    # Exclude VNTRs that did not have an eQTL signal
+    data = data.loc[data["eQTL"] == "Yes"]
+    data["chrom_start"] = data["VNTR Coordinates (GRCh38)"].apply(lambda x: x.split("-")[0])
+    data["chrom"] = data["chrom_start"].apply(lambda x: x.split(":")[0])
+    data["start"] = data["chrom_start"].apply(lambda x: x.split(":")[1]).astype(int)
+    return data
+
+def read_gwas(filename):
+    columns = "phenotype,POS,ID,REF,ALT,PROVISIONAL_REF?,A1,OMITTED,A1_FREQ,TEST,OBS_CT,BETA,SE,T_STAT,P,ERRCODE".split(",")
+    data = pd.read_csv(filename, sep="\t", header=0, names=columns, index_col=False)
+    data["chrom_start"] = data["ID"].apply(lambda x: x.replace("_", ":"))
+    data["chrom"] = data["chrom_start"].apply(lambda x: x.split(":")[0])
+    data["start"] = data["chrom_start"].apply(lambda x: x.split(":")[1]).astype(int)
+    return data
+
+def main():
+    e_vntrs = read_e_vntrs(filename="41467_2021_22206_MOESM3_ESM.xlsx")
+    gwas_sig_hits = read_gwas("results_v4_blood_significant_hits.tab")
+    #gwas_sig_hits = read_gwas("results_v3_eur_significant_hits.tab")
+    print(f"Working with {len(e_vntrs)} e-vntrs and {len(gwas_sig_hits)} significant tr-gwas hits")
+    gwas_e_vntrs = gwas_sig_hits.merge(e_vntrs, how="inner", on="chrom_start")
+    print(gwas_e_vntrs[["chrom_start", "Nearest Gene", "phenotype", "P"]])
+
+if __name__ == "__main__":
+    main()
