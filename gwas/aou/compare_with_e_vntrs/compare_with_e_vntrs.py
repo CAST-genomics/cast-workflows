9dbf65feb05cba5eb961ade00332697537f7f02e
sara-javadzadeh <sarajno@gmail.com>
2025-05-12 18:18:39 +0000

    Save progress

--- gwas/aou/compare_with_e_vntrs/compare_with_e_vntrs.py
+++ gwas/aou/compare_with_e_vntrs/compare_with_e_vntrs.py
@@ -12,8 +12,11 @@ def read_e_vntrs(filename):
     data["start"] = data["chrom_start"].apply(lambda x: x.split(":")[1]).astype(int)
     return data
 
-def read_gwas(filename):
-    columns = "phenotype,POS,ID,REF,ALT,PROVISIONAL_REF?,A1,OMITTED,A1_FREQ,TEST,OBS_CT,BETA,SE,T_STAT,P,ERRCODE".split(",")
+def read_gwas(filename, is_binary):
+    if is_binary:
+        columns = "phenotype,POS,ID,REF,ALT,PROVISIONAL_REF?,A1,OMITTED,A1_FREQ,FIRTH?,TEST,OBS_CT,OR,LOG(OR)_SE,Z_STAT,P,ERRCODE".split(",")
+    else:
+        columns = "phenotype,POS,ID,REF,ALT,PROVISIONAL_REF?,A1,OMITTED,A1_FREQ,TEST,OBS_CT,BETA,SE,T_STAT,P,ERRCODE".split(",")
     data = pd.read_csv(filename, sep="\t", header=0, names=columns, index_col=False)
     data["chrom_start"] = data["ID"].apply(lambda x: x.replace("_", ":"))
     data["chrom"] = data["chrom_start"].apply(lambda x: x.split(":")[0])
@@ -22,17 +25,16 @@ def read_gwas(filename):
 
 def main():
     e_vntrs = read_e_vntrs(filename="41467_2021_22206_MOESM3_ESM.xlsx")
-    #gwas_sig_hits = read_gwas("results_v4_blood_significant_hits.tab")
-    #gwas_sig_hits = read_gwas("results_v3_eur_significant_hits.tab")
-    gwas_sig_hits = read_gwas("results_v2_all_significant_hits.tab")
+    #gwas_sig_hits = read_gwas("results_v4_blood_significant_hits.tab", is_binary=False)
+    #gwas_sig_hits = read_gwas("results_v3_eur_significant_hits.tab", is_binary=True)
+    gwas_sig_hits = read_gwas("results_v2_all_significant_hits.tab", is_binary=True)
     print(f"Working with {len(e_vntrs)} e-vntrs and {len(gwas_sig_hits)} significant tr-gwas hits")
     gwas_e_vntrs = gwas_sig_hits.merge(e_vntrs, how="inner", on="chrom_start")
     print(gwas_e_vntrs[["chrom_start", "Nearest Gene", "phenotype", "P"]])
 
     # A one-off check for a Schizophrenia specific VNTR in the literature
     gwas_mir_137_dist = gwas_sig_hits.loc[gwas_sig_hits["chrom"] == "chr1"]["start"].apply(lambda x: abs(x - 98046174))
-
-    print("Distances of VNTRs with significant hit to the MIR137 VNTR: ", gwas_mir_137_dist.sort_values())
+    #print("Distances of VNTRs with significant hit to the MIR137 VNTR: ", gwas_mir_137_dist.sort_values())
 
 if __name__ == "__main__":
     main()
--- gwas/aou/run_gwas_vntrs.sh
+++ gwas/aou/run_gwas_vntrs.sh
@@ -2,20 +2,30 @@
 
 ref="data/lrwgs_p_g_polymorphic_vntrs_sr_6_ml_95.sorted.vcf.gz"
 
+
+
 # Disease
 samples="samples/EUR_WHITE.csv"
 for chr in "chr16"; do
     aou_220k_imputed="../../imputation/wdl/data/imputed_${chr}.annotated.rh.vcf.gz"
     for phenotype in "Malignant_neoplasm_of_the_skin"; do
-    #for phenotype in "Hypercholesterolemia"; do
+    #for phenotype in "Hyperlipidemia"; do
+    #for phenotype in "glaucoma"; do
             echo "Running gwas for $phenotype"
-            vntr_plink_gwas_file="../../../rebased_cast-workflows/cast-workflows/tr-gwas/results_v3_eur_significant_in_v2/sara_${phenotype}_EUR_WHITE_gwas.tab"
+            python plot_genotype_phenotype.py --phenotype $phenotype \
+                   --tr-vcf $aou_220k_imputed \
+                   --samples $samples \
+                   --outdir outputs/${chr} \
+                   --annotations annotation_points.csv \
+                   --binary
+           : '
+             vntr_plink_gwas_file="../../../rebased_cast-workflows/cast-workflows/tr-gwas/results_v3_eur_significant_in_v2/sara_${phenotype}_EUR_WHITE_gwas.tab"
             processed_vntr_gwas_file="data/plink/processed_gwas_${phenotype}.tab"
-            cat $vntr_plink_gwas_file | grep '^#CHROM' | head -n 1 > $processed_vntr_gwas_file
-            cat $vntr_plink_gwas_file | grep 'ADD' >> $processed_vntr_gwas_file
-            phecode="CA_103"
+            cat $vntr_plink_gwas_file | grep "^#CHROM" | head -n 1 > $processed_vntr_gwas_file
+            cat $vntr_plink_gwas_file | grep "ADD" >> $processed_vntr_gwas_file
+            phecode="EM_239.1"
             snp_gwas_file="data/all_by_all/df_dump_${chr}_${phecode}.csv"
-            ./aou_gwas.py --phenotype $phenotype \
+                ./aou_gwas.py --phenotype $phenotype \
                    --num-pcs 10 \
                    --method associaTR \
                    --tr-vcf $aou_220k_imputed \
@@ -24,9 +34,9 @@ for chr in "chr16"; do
                    --annotations annotation_points.csv \
                    --snp-gwas-file $snp_gwas_file \
                    --vntr-gwas-file $processed_vntr_gwas_file \
+                   --plot-genotype-phenotype \
                    --binary \
-                   --plot
-                  # --plot-genotype-phenotype \
+                   --plot'
     done
 done
 
@@ -41,7 +51,14 @@ for chr in "chr1"; do
         aou_220k_imputed="../../imputation/wdl/data/imputed_${chr}.annotated.rh.vcf.gz"
         #snp_gwas_file="data/all_by_all/df_dump_${chr}_${phecode}.csv"
         echo "Running gwas for $phenotype"
-        ./aou_gwas.py --phenotype $phenotype \
+        python plot_genotype_phenotype.py --phenotype $phenotype \
+           --tr-vcf $aou_220k_imputed \
+           --samples $samples \
+           --outdir outputs/${chr} \
+           --annotations annotation_points.csv \
+           --norm quantile
+
+       : " ./aou_gwas.py --phenotype $phenotype \
            --num-pcs 10 \
            --method associaTR \
            --tr-vcf $aou_220k_imputed \
@@ -51,7 +68,7 @@ for chr in "chr1"; do
            --plot-genotype-phenotype \
            --norm quantile \
            --plot
-           #--snp-gwas-file $snp_gwas_file \
+           #--snp-gwas-file $snp_gwas_file "
     done
 done
 
@@ -59,5 +76,3 @@ exit 0
 
 
 #most_significant_hit=$(tail -n +4 outputs/${chr}/${phenotype}_associaTR_${samples_prefix}.gwas.tab | cut -f6 | sort -g  | awk NF | head -n 1)
-#echo "most_significant_hit for phenotype $phenotype is $most_significant_hit" >> $summary
-#echo "----------- most_significant_hit for phenotype $phenotype is $most_significant_hit"
