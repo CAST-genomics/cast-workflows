6ce8b7adea902abf9f0af0ae7e6eb269ade7670e
nichole <nicholema621@gmail.com>
2025-07-10 09:08:46 -0700

    add more qc filters

--- gwas/aou/subset_hail_to_plink.py
+++ gwas/aou/subset_hail_to_plink.py
@@ -16,13 +16,29 @@ mt_wgs_path = os.getenv("WGS_ACAF_THRESHOLD_MULTI_HAIL_PATH")
 mt = hl.read_matrix_table(mt_wgs_path)
 mt = hl.filter_intervals(mt, [hl.parse_locus_interval(region,)])
 
+
+
 # Keep only bi-allelic variants
-mt = mt.filter_rows(hl.len(mt.alleles) == 2, keep=True)
+#mt = mt.filter_rows(hl.len(mt.alleles) == 2, keep=True)
 
 # Compute variant QC metrics to get MAF
 mt = hl.variant_qc(mt)
 
 # Filter to MAF > 5%
-mt = mt.filter_rows(mt.variant_qc.AF[1] > 0.05, keep=True)
+#mt = mt.filter_rows(mt.variant_qc.AF[1] > 0.05, keep=True)
+
+
+#Add other QC filters
+mt = mt.annotate_entries(FT = hl.coalesce(mt.FT,'PASS'))
+mt = mt.filter_entries(mt.FT =='PASS')
+mt = mt.filter_entries(mt.GQ >= 20) #20
+
+ # Locus and Sample QC
+mt = hl.variant_qc(mt)
+mt = hl.sample_qc(mt)
+mt = mt.filter_cols(mt.sample_qc.call_rate >= 0.9, keep = True) #0.9
+mt = mt.filter_rows(mt.variant_qc.call_rate >= 0.9, keep = True) #0.9
+mt = mt.filter_rows(hl.min(mt.variant_qc.AF) > 0.01, keep = True) #0.01
+mt = mt.filter_rows(mt.variant_qc.p_value_hwe > 1e-100, keep = True) # 1e-100
 
 hl.export_plink(mt, outfile, ind_id = mt.s)
